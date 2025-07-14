8485d2beb4c8891d7952d62a038c8979ae0de84d
rasyarui <rasyajago12@gmail.com>
2025-07-15 02:02:29 +0700

    test

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -14,7 +14,7 @@
             <form wire:submit.prevent="loginUser()">
 
                 <div class="username">
-                    <label for="username">Username</label>
+                    <label for="username">Usernamess</label>
                     <input type="text" name="username" id="username" placeholder="Username" wire:model.live="username"
                         requigit red>
                     @error('username')
